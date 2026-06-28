bff11eed671849130bfdfd4e4edd238ffbd83a20
Uilson5023 <73534894+Uilson5023@users.noreply.github.com>
2026-06-28 19:18:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -448,7 +448,6 @@ def admin_recalcular_tudo():
     flash("Todas as pontuações foram recalculadas!", "success")
     return redirect(url_for("admin_resultados"))
 
-
+init_db()
 if __name__ == "__main__":
-    init_db()
-    app.run(debug=True, host="0.0.0.0", port=5000)
+     app.run(debug=True, host="0.0.0.0", port=5000)
